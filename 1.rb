63de49e8f2f198f38ed7eaac36e83baa220715ec
Pquips <amirrose4@gmail.com>
2018-03-29 15:22:52 -0700

    Update 1.rb

--- 1.rb
+++ 1.rb
@@ -1,13 +1,17 @@
+# Song Name:Pokemon Theme
+
+# Sheet Music: https://musescore.com/user/6217456/scores/2525106
+
+# BPM Used: I modified it to my liking
+
+# These are my variables
 x = :piano
-
 z = :pretty_bell
-
 b = 110
-
 a = 1
-
 sfx = "C:/Users/amir_roseaminifu/Desktop/Sample/pika.wav"
 
+# This is a function for my first measure
 define :fm do
   play :G3, amp: a
   play :Bb3, amp: a
@@ -15,6 +19,7 @@ define :fm do
   sleep 0.5
 end
 
+# This is a function for my second measure
 define :sm do
   play :F3, amp: a
   play :A3, amp: a
@@ -22,22 +27,17 @@ define :sm do
   sleep 0.5
 end
 
+# This is a loop that speeds up gradually
 4.times do
-  
   with_synth x do
-    
     use_bpm b
-    
     8.times do
-      fm
+      fm #Here I use the functions
     end
     8.times do
       sm
     end
-    
     a = (a+0.125)
     b = (b+10)
-    
   end
-  
 end
