c25257052f989793877e8e40f41ad5d3a97559b0
Pquips <amirrose4@gmail.com>
2018-03-29 15:44:12 -0700

    Update 5.rb

--- 5.rb
+++ 5.rb
@@ -1,22 +1,25 @@
+# Song Name:Gleen
+# Sheet Music: Custom transposition
+# BPM Used: 150
+
+# These are my variables
 b = 150
-
 a = 3
-
 c = 1.5
-
 use_bpm b
-
 melody = "C:/Users/amir_roseaminifu/Desktop/tonight/Sample/melody.wav"
 
+# This is a loop that plays my melody for 32 beats
 1.times do
   sample melody, amp: c
-  
 end
 
+# This is a function that i use for sleeps
 define :s do |c|
   sleep c
 end
 
+# This is a liveloop that plays the baseline gradually fading out
 6.times do
   sample :bd_boom, amp: a
   s 1.5
