05ed9473b52105b8d597d84a1128a1604ce73b46
Pquips <amirrose4@gmail.com>
2018-03-29 15:41:52 -0700

    Update 4.rb

--- 4.rb
+++ 4.rb
@@ -1,20 +1,21 @@
+c
 b = 150
-
 melody = "C:/Users/amir_roseaminifu/Desktop/tonight/Sample/premelody.wav"
-
 use_bpm b
 
+# This is a live loop that plays my sample on repeat
 live_loop :melody do
   sample melody, amp: 1.5
   sleep 32
 end
 
+# This is an example of a parameterized function that I use for sleeps
 define :s do |c|
   sleep c
 end
 
+# This is a live loop where I repeat my bassline using my parameterized function
 live_loop :bassline do
-  
   2.times do
     sample :bd_boom, amp: 3
     s 1.5
@@ -29,7 +30,6 @@ live_loop :bassline do
     sample :bd_boom, amp: 3
     s 1
   end
-  
   sample :bd_boom, amp: 3
   s 0.50
   sample :bd_boom, amp: 3
